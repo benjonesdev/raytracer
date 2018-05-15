11e47fdd9f39bd06b6cde26ab1beae1624b5cf9c
Ben Jones <benjones@berkeley.edu>
2018-05-15 00:34:51 -0700

    add anti-aliasing

--- src/sphere.h
+++ src/sphere.h
@@ -6,13 +6,13 @@
 class sphere: public hitable {
     public:
         sphere() {}
-        sphere(vec3 cent, float r) : center(cen), radius(r) {};
+        sphere(vec3 cen, float r) : center(cen), radius(r) {};
         virtual bool hit(const ray& r, float tmin, float tmax, hit_record& rec) const;
         vec3 center;
         float radius;
 };
 
-bool sphere::hit(const ray&, float t_min, float t_max, hit_record& rec) const {
+bool sphere::hit(const ray& r, float t_min, float t_max, hit_record& rec) const {
     vec3 oc = r.origin() - center;
     float a = dot(r.direction(), r.direction());
     float b = dot(oc, r.direction());
