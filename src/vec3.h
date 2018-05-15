06a66a67d505022f9b8235e7ad4b40d49f957631
Ben Jones <benjones@berkeley.edu>
2018-05-14 22:42:02 -0700

    visualize surface normals

--- src/vec3.h
+++ src/vec3.h
@@ -59,7 +59,7 @@ inline vec3 operator+(const vec3 &v1, const vec3 &v2) {
 
 // Support subtracting vec3 from another vec3
 inline vec3 operator-(const vec3 &v1, const vec3 &v2) {
-    return vec3(v1.e[0] + v2.e[0], v1.e[1] + v2.e[1], v1.e[2] + v2.e[2]);
+    return vec3(v1.e[0] - v2.e[0], v1.e[1] - v2.e[1], v1.e[2] - v2.e[2]);
 }
 
 // Support multiplying vec3 with another vec3
